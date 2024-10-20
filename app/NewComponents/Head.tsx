fb8bf6606079c541eff9590f97c61bbdcc19960f
Surendra Kumar <147039763+surendrakumar6350@users.noreply.github.com>
2024-10-19 21:25:14 -0800

    [update]-Open-Graph-Image-Added-Head-Tag

--- app/NewComponents/Head.tsx
+++ app/NewComponents/Head.tsx
@@ -3,10 +3,26 @@ import React from 'react'
 const Head = () => {
   return (
     <head>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  </head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/x-icon" href="/favicon-32x32.png" />
+      <link rel="canonical" href="https://shekhawati-kaa-data.online" />
+      <meta name="keywords" content="college students, find students near me, college student locator, connect with students, student directory"></meta>
+
+      <meta property="og:url" content="https://shekhawati-kaa-data.online" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Find College Students Near You" />
+      <meta property="og:description" content="Easily find college students near you by using our convenient search tool to connect with individuals in your area based on location, pincode, and more." />
+      <meta property="og:image" content="https://shekhawati-kaa-data.online/ogimage.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="shekhawati-kaa-data.online" />
+      <meta property="twitter:url" content="https://shekhawati-kaa-data.online" />
+      <meta name="twitter:title" content="Find College Students Near You" />
+      <meta name="twitter:description" content="Easily find college students near you by using our convenient search tool to connect with individuals in your area based on location, pincode, and more." />
+      <meta name="twitter:image" content="https://shekhawati-kaa-data.online/ogimage.png"></meta>
+    </head>
   )
 }
 
