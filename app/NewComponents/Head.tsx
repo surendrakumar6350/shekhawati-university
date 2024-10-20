15be73546017f326bae976bc22b95ae6d383cf8f
Surendra Kumar <147039763+surendrakumar6350@users.noreply.github.com>
2024-10-19 20:56:12 -0800

    [Fix]-In-Head-Section-Favicon-changed

---
+++ app/NewComponents/Head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  </head>
+  )
+}
+
+export default Head
