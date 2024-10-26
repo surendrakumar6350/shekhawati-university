7e96f71582313cdd61d3f82185f01f0b64f78ff4
Surendra Kumar <147039763+surendrakumar6350@users.noreply.github.com>
2024-10-26 04:12:25 -0800

    [Fix]-Bot-Sending-One-SMS-Multi-Time

--- app/api/postclick/route.js
+++ app/api/postclick/route.js
@@ -2,22 +2,21 @@ import { connectDb } from "@/dbconnection/connect";
 import { NextResponse } from "next/server";
 import { click } from "@/dbconnection/Schemas/click";
 import { signup } from "@/dbconnection/Schemas/signup";
-import { headers } from 'next/headers'
+import { headers } from 'next/headers';
 import rateLimit from "../../../rateLimit";
 import { UAParser } from 'ua-parser-js';
 import { sendMessage } from "@/utils/functions";
 
 export async function POST(req) {
-  const headersList = headers()
-  const ip = headersList.get('x-forwarded-for')
-  const ankit = headersList.get('user-agent')
+  const headersList = headers();
+  const ip = headersList.get('x-forwarded-for');
+  const ankit = headersList.get('user-agent');
   const parser = new UAParser(ankit);
   let parserResults = parser.getResult();
 
   const usercookie = await req?.cookies?.get("user");
   let data = await req.json();
-  const search = {search: data};
-  await sendMessage(search);
+  const search = { search: data };
 
   const userid = usercookie?.value;
   if (!userid) {
@@ -33,15 +32,15 @@ export async function POST(req) {
     if (!res) {
       return NextResponse.json({ sucess: false });
     } else {
-      const resObj =  await rateLimit(res._id);
-      if(!resObj.success) {
-        return NextResponse.json({success: false, message: "Rate limit exceeded"});
+      const resObj = await rateLimit(res._id);
+      if (!resObj.success) {
+        return NextResponse.json({ success: false, message: "Rate limit exceeded" });
       }
-      let obj = {search: data, user: res, ip: ip, device: parserResults}
+      let obj = { search: data, user: res, ip: ip, device: parserResults }
       const ress = new click(obj);
       const savedclick = await ress.save();
       await sendMessage(search);
-      return NextResponse.json({success: true});
+      return NextResponse.json({ success: true });
     }
   } catch (error) {
     console.log(error);
