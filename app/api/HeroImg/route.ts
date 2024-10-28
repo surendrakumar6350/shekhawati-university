a38838290337e9a73d17b81ad42618ec7658f332
Surendra Kumar <147039763+surendrakumar6350@users.noreply.github.com>
2024-10-27 20:54:06 -0800

    [Update]-Hero-Section-Img-Changed

--- app/api/HeroImg/route.ts
+++ app/api/HeroImg/route.ts
@@ -4,10 +4,10 @@ export async function POST(request: Request): Promise<NextResponse> {
   return NextResponse.json({
     success: true,
     heroImages: [
-      "https://exam.shekhauniexam.in/Image_2024/Photo/19e0d5d5-483b-470a-aed7-e0e293365b32.jpg",
-      "https://exam.shekhauniexam.in/Image_2024/Photo/686ad74a-51d1-42cf-b7d7-c194c1f7b1f9.jpg",
-      "https://exam.shekhauniexam.in/Image_2024/Photo/30a2f466-cfc2-48df-97d6-5d55f7f659cc.jpg",
-      "https://exam.shekhauniexam.in/Image_2024/Photo/ea424317-fc2a-4b1a-8fe5-0f25f7733310.jpg",
+      "/istockphoto-1128932923-612x612.jpg",
+      "/istockphoto-1359499268-612x612.jpg",
+      "/istockphoto-1141737652-612x612.jpg",
+      "/istockphoto-1309256708-612x612.jpg",
     ],
   });
 }
